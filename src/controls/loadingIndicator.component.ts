a825847a3739566db367ca706ed6f6d06904dc4b
Richard Collier <ergosoftuk@gmail.com>
2017-08-23 18:48:42 +0100

    Allow setting of indicator text

--- src/controls/loadingIndicator.component.ts
+++ src/controls/loadingIndicator.component.ts
@@ -3,10 +3,11 @@ import {Component, Input} from "@angular/core";
 @Component({
 	selector : 'loadingIndicator',
 	template : `<div class="loading" [hidden]="done">
-		<div class="loadingCaption"><i class="fa fa-spinner fa-spin"></i> Loading...</div>
+		<div class="loadingCaption"><i class="fa fa-spinner fa-spin"></i> {{text}}</div>
 	</div>
 	<div [hidden]="!done"><ng-content></ng-content></div>`
 })
 export class LoadingIndicatorComponent {
+	@Input() text : string = 'Loading...';
 	@Input() done : any;
 }
